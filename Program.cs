5129b31117cdd466d86366b8b8354f45636ca4c8
mapovihep <mromax161@mail.ru>
2022-07-24 21:48:30 +0300

    deleted razor env

--- Program.cs
+++ Program.cs
@@ -13,23 +13,10 @@ builder.Services.AddSingleton<ITransactionMapper, TransactionMapper>();
 var app = builder.Build();
 app.UseRouting();
 
-// Configure the HTTP request pipeline.
-/*if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}*/
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
 app.UseRouting();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
 app.UseAuthorization();
-
-//app.MapRazorPages();
 
 app.Run("http://localhost:3000");
